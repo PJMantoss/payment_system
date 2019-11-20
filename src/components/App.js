fb21df35cf3baaca3453d20a0e57ad71990789de
PJMantoss <joelptoss@gmail.com>
2019-11-20 15:34:35 +0100

    Update App.js

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { Router, Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import Products from './Products'
+import Checkout from './Checkout'
+import { products } from '../products'
+
+const history = createBrowserHistory()
+
+const App = () => {
+    const [selectedProduct, setSelectedProduct] = useState(null)
+
+    return(
+        <Router>
+            <Switch>
+                <Route />
+                <Route />
+            </Switch>
+        </Router>
+    )
+}
